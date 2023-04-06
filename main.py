b884572bae634b0753b344042c5ce2b442dda043
pengyuansun <pengyuan.sun@siemens,com>
2023-04-06 16:53:29 +0800

    generate dataset

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 import pandas as pd
 from tqdm.auto import tqdm
+import random
 
 
 STATUS_INDICATOR = 1
 SEQUENCE_LENGTH = 15
+BATCH_SIZE = 32
 
 data_file = r"./data/train_data.csv"
 train_df = pd.read_csv(data_file)
@@ -35,3 +37,15 @@ for idx in tqdm(range(len(train_df))):
 train_inputs_tensor = tf.concat(train_inputs, axis=0)
 train_targets_tensor = tf.concat(train_targets, axis=0)
 
+sample_length = len(train_df) - SEQUENCE_LENGTH
+train_length = int(sample_length * 0.8)
+indices = list(range(sample_length))
+random.shuffle(indices)
+
+model_train_inputs = tf.gather(train_inputs_tensor, indices[0: train_length], axis=0)
+model_val_inputs = tf.gather(train_inputs_tensor, indices[train_length:], axis=0)
+model_train_targets = tf.gather(train_targets_tensor, indices[0: train_length], axis=0)
+model_val_targets = tf.gather(train_targets_tensor, indices[train_length:], axis=0)
+
+train_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_train_inputs, 'targets': model_train_targets}).shuffle(2048).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+val_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_val_inputs, 'targets': model_val_targets}).shuffle(2048).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
