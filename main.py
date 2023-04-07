64371e50336b9c01a6e8bf442a4b46c22686652e
SunPengYuan <pengyuan.sun@siemens.com>
2023-04-07 13:13:33 +0800

    finish base structure

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-import tensorflow as tf
-import pandas as pd
-from tqdm.auto import tqdm
 import random
 
+import pandas as pd
+import tensorflow as tf
+from tqdm.auto import tqdm
+from model import TFSModel, train_step, model_evaluate
 
 STATUS_INDICATOR = 1
 SEQUENCE_LENGTH = 15
 BATCH_SIZE = 32
+
+# Train parameters
+EPOCH = 1000
+EVALUATE_INTERVAL = 7
 
 data_file = r"./data/train_data.csv"
 train_df = pd.read_csv(data_file)
@@ -31,8 +36,8 @@ for idx in tqdm(range(len(train_df))):
         break
     else:
         train_inputs.append(tf.expand_dims(tf.convert_to_tensor(train_df.iloc[idx: idx + 15], dtype=tf.float32), axis=0))
-        train_target = 0 if train_df.iloc[idx + 15, 7] < -STATUS_INDICATOR else 1 if train_df.iloc[idx + 15, 7] < STATUS_INDICATOR else 2
-        train_targets.append(tf.expand_dims(tf.expand_dims(tf.convert_to_tensor(train_target, dtype=tf.int8), axis=0), axis=0))
+        train_target = [1, 0, 0] if train_df.iloc[idx + 15, 7] < -STATUS_INDICATOR else [0, 1, 0] if train_df.iloc[idx + 15, 7] < STATUS_INDICATOR else [0, 0, 1]
+        train_targets.append(tf.expand_dims(tf.convert_to_tensor(train_target, dtype=tf.int8), axis=0))
 
 train_inputs_tensor = tf.concat(train_inputs, axis=0)
 train_targets_tensor = tf.concat(train_targets, axis=0)
@@ -48,4 +53,28 @@ model_train_targets = tf.gather(train_targets_tensor, indices[0: train_length], 
 model_val_targets = tf.gather(train_targets_tensor, indices[train_length:], axis=0)
 
 train_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_train_inputs, 'targets': model_train_targets}).shuffle(2048).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
-val_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_val_inputs, 'targets': model_val_targets}).shuffle(2048).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+val_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_val_inputs, 'targets': model_val_targets}).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+
+model = TFSModel()
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.0005)
+loss = tf.keras.losses.CategoricalCrossentropy()
+loss_metrics = tf.keras.metrics.Mean()
+
+for epoch in range(EPOCH):
+    print(f'epoch {epoch}:')
+    batch_idx = 0
+    for inputs in train_ds:
+        train_step(model=model,
+                   optimizer=optimizer,
+                   loss=loss,
+                   loss_metrics=loss_metrics,
+                   batched_inputs=inputs['inputs'],
+                   batched_targets=inputs['targets'])
+
+        if batch_idx % EVALUATE_INTERVAL == 0:
+            model_evaluate(model, val_ds)
+
+        batch_idx += 1
+        print(f"train loss is: {loss_metrics.result().numpy()}")
+
+    loss_metrics.reset_states()
--- model.py
+++ model.py
@@ -1,13 +1,48 @@
 import tensorflow as tf
+import math
+import numpy as np
 
 
 class TFSModel(tf.keras.Model):
     def __init__(self):
         super().__init__()
-        self.lstm = tf.keras.layers.LSTM(50)
+        self.lstm = tf.keras.layers.LSTM(64)
+        self.regressor = tf.keras.layers.Dense(3)
+        self.softmax = tf.keras.layers.Softmax()
 
     def call(self, inputs, training=False):
-        pass
+        lstm_outputs = self.lstm(inputs)  # [batch_size, 20]
+        dense_outputs = self.regressor(lstm_outputs)  # [batch_size, 3]
+        outputs = self.softmax(dense_outputs)  # [batch_size, 3]
+        return outputs
 
     def get_config(self):
         pass
+
+
+@tf.function()
+def train_step(model, optimizer, loss, loss_metrics, batched_inputs, batched_targets):
+    with tf.GradientTape() as tape:
+        predictions = model(inputs=batched_inputs, training=True)
+        loss = loss(batched_targets, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    loss_metrics(loss)
+
+
+def model_evaluate(model, dataset):
+    predictions = []
+    targets = []
+
+    for inputs in dataset:
+        prediction = model(inputs=inputs['inputs'], training=False)
+
+        predictions.append(prediction)
+        targets.append(inputs['targets'])
+
+    predictions = tf.concat(predictions, axis=0)
+    targets = tf.concat(targets, axis=0)
+
+    evaluate_loss = np.mean(tf.keras.losses.mean_squared_error(targets, predictions).numpy())
+
+    print(f'evaluate loss is {math.sqrt(evaluate_loss):.5f}')
