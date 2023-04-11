aae45571a7870a7359e9ca08275780d26d806c2e
pengyuansun <pengyuan.sun@siemens,com>
2023-04-11 14:53:31 +0800

    start to add features

--- data.py
+++ data.py
@@ -4,49 +4,57 @@ import pandas as pd
 import tensorflow as tf
 from tqdm.auto import tqdm
 
+from config import SEQUENCE_LENGTH, STATUS_INDICATOR, BATCH_SIZE
 
-SEQUENCE_LENGTH = 20
-STATUS_INDICATOR = 1
-BATCH_SIZE = 64
 
-data_file = r"./data/train_data.csv"
-train_df = pd.read_csv(data_file)
+def build_ds():
+    data_file = r"./data/train_data.csv"
+    train_df = pd.read_csv(data_file)
 
-drop_rows = (train_df.loc[train_df.loc[:, 'volume'] == 0]).index.tolist() + [0]
-drop_columns = ['date', 'code', 'isST', 'adjustflag', 'tradestatus'] + ['peTTM', 'psTTM', 'pcfNcfTTM', 'pbMRQ']
+    drop_rows = (train_df.loc[train_df.loc[:, 'volume'] == 0]).index.tolist() + [0]
+    drop_columns = ['date', 'code', 'isST', 'adjustflag', 'tradestatus'] + ['peTTM', 'psTTM', 'pcfNcfTTM', 'pbMRQ']
 
-train_df.drop(drop_columns, axis=1, inplace=True)
-train_df.drop(drop_rows, inplace=True)
+    train_df.drop(drop_columns, axis=1, inplace=True)
+    train_df.drop(drop_rows, inplace=True)
 
-volume_adjust_factor = 1e7
-amount_adjust_factor = 1e8
+    volume_adjust_factor = 1e7
+    amount_adjust_factor = 1e8
 
-train_df.loc[:, 'volume'] /= volume_adjust_factor
-train_df.loc[:, 'amount'] /= amount_adjust_factor
+    train_df.loc[:, 'volume'] /= volume_adjust_factor
+    train_df.loc[:, 'amount'] /= amount_adjust_factor
 
-train_inputs = []
-train_targets = []
+    train_inputs = []
+    train_targets = []
 
-for idx in tqdm(range(len(train_df))):
-    if idx > len(train_df) - 1 - SEQUENCE_LENGTH:
-        break
-    else:
-        train_inputs.append(tf.expand_dims(tf.convert_to_tensor(train_df.iloc[idx: idx + 15], dtype=tf.float32), axis=0))
-        train_target = [1, 0, 0] if train_df.iloc[idx + 15, 7] < -STATUS_INDICATOR else [0, 1, 0] if train_df.iloc[idx + 15, 7] < STATUS_INDICATOR else [0, 0, 1]
-        train_targets.append(tf.expand_dims(tf.convert_to_tensor(train_target, dtype=tf.int8), axis=0))
+    for idx in tqdm(range(len(train_df))):
+        if idx > len(train_df) - 1 - SEQUENCE_LENGTH:
+            break
+        else:
+            train_inputs.append(
+                tf.expand_dims(tf.convert_to_tensor(train_df.iloc[idx: idx + 15], dtype=tf.float32), axis=0))
+            train_target = [1, 0, 0] if train_df.iloc[idx + 15, 7] < -STATUS_INDICATOR else [0, 1, 0] if train_df.iloc[
+                                                                                                             idx + 15, 7] < STATUS_INDICATOR else [
+                0, 0, 1]
+            train_targets.append(tf.expand_dims(tf.convert_to_tensor(train_target, dtype=tf.int8), axis=0))
 
-train_inputs_tensor = tf.concat(train_inputs, axis=0)
-train_targets_tensor = tf.concat(train_targets, axis=0)
+    train_inputs_tensor = tf.concat(train_inputs, axis=0)
+    train_targets_tensor = tf.concat(train_targets, axis=0)
 
-sample_length = len(train_df) - SEQUENCE_LENGTH
-train_length = int(sample_length * 0.8)
-indices = list(range(sample_length))
-random.shuffle(indices)
+    sample_length = len(train_df) - SEQUENCE_LENGTH
+    train_length = int(sample_length * 0.8)
+    indices = list(range(sample_length))
+    random.shuffle(indices)
 
-model_train_inputs = tf.gather(train_inputs_tensor, indices[0: train_length], axis=0)
-model_val_inputs = tf.gather(train_inputs_tensor, indices[train_length:], axis=0)
-model_train_targets = tf.gather(train_targets_tensor, indices[0: train_length], axis=0)
-model_val_targets = tf.gather(train_targets_tensor, indices[train_length:], axis=0)
+    model_train_inputs = tf.gather(train_inputs_tensor, indices[0: train_length], axis=0)
+    model_val_inputs = tf.gather(train_inputs_tensor, indices[train_length:], axis=0)
+    model_train_targets = tf.gather(train_targets_tensor, indices[0: train_length], axis=0)
+    model_val_targets = tf.gather(train_targets_tensor, indices[train_length:], axis=0)
 
-train_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_train_inputs, 'targets': model_train_targets}).shuffle(2048).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
-val_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_val_inputs, 'targets': model_val_targets}).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+    train_ds = tf.data.Dataset.from_tensor_slices(
+        {'inputs': model_train_inputs, 'targets': model_train_targets}).shuffle(2048).batch(BATCH_SIZE).prefetch(
+        tf.data.experimental.AUTOTUNE)
+    val_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_val_inputs, 'targets': model_val_targets}).batch(
+        BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+
+    return train_ds, val_ds
+
--- main.py
+++ main.py
@@ -1,60 +1,14 @@
-import random
-
-import pandas as pd
 import tensorflow as tf
-from tqdm.auto import tqdm
-from model import TFSModel, train_step, model_evaluate
 
-STATUS_INDICATOR = 3
-SEQUENCE_LENGTH = 20
-BATCH_SIZE = 64
+from model import TFSModel, train_step, model_evaluate
+from data import build_ds
 
 # Train parameters
 EPOCH = 1000
 EVALUATE_INTERVAL = 7
-LEARNING_RATE = 0.003
+LEARNING_RATE = 0.00003
 
-data_file = r"./data/train_data.csv"
-train_df = pd.read_csv(data_file)
-
-drop_rows = (train_df.loc[train_df.loc[:, 'volume'] == 0]).index.tolist() + [0]
-drop_columns = ['date', 'code', 'isST', 'adjustflag', 'tradestatus'] + ['peTTM', 'psTTM', 'pcfNcfTTM', 'pbMRQ']
-
-train_df.drop(drop_columns, axis=1, inplace=True)
-train_df.drop(drop_rows, inplace=True)
-
-volume_adjust_factor = 1e7
-amount_adjust_factor = 1e8
-
-train_df.loc[:, 'volume'] /= volume_adjust_factor
-train_df.loc[:, 'amount'] /= amount_adjust_factor
-
-train_inputs = []
-train_targets = []
-
-for idx in tqdm(range(len(train_df))):
-    if idx > len(train_df) - 1 - SEQUENCE_LENGTH:
-        break
-    else:
-        train_inputs.append(tf.expand_dims(tf.convert_to_tensor(train_df.iloc[idx: idx + 15], dtype=tf.float32), axis=0))
-        train_target = [1, 0, 0] if train_df.iloc[idx + 15, 7] < -STATUS_INDICATOR else [0, 1, 0] if train_df.iloc[idx + 15, 7] < STATUS_INDICATOR else [0, 0, 1]
-        train_targets.append(tf.expand_dims(tf.convert_to_tensor(train_target, dtype=tf.int8), axis=0))
-
-train_inputs_tensor = tf.concat(train_inputs, axis=0)
-train_targets_tensor = tf.concat(train_targets, axis=0)
-
-sample_length = len(train_df) - SEQUENCE_LENGTH
-train_length = int(sample_length * 0.8)
-indices = list(range(sample_length))
-random.shuffle(indices)
-
-model_train_inputs = tf.gather(train_inputs_tensor, indices[0: train_length], axis=0)
-model_val_inputs = tf.gather(train_inputs_tensor, indices[train_length:], axis=0)
-model_train_targets = tf.gather(train_targets_tensor, indices[0: train_length], axis=0)
-model_val_targets = tf.gather(train_targets_tensor, indices[train_length:], axis=0)
-
-train_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_train_inputs, 'targets': model_train_targets}).shuffle(2048).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
-val_ds = tf.data.Dataset.from_tensor_slices({'inputs': model_val_inputs, 'targets': model_val_targets}).batch(BATCH_SIZE).prefetch(tf.data.experimental.AUTOTUNE)
+train_ds, val_ds = build_ds()
 
 model = TFSModel()
 optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
